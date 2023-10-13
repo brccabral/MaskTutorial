de33fe36805044aecd295d3d3e3d8d2be4718f61
Bruno Cabral <cabral.brc@gmail.com>
2023-10-13 10:46:35 -0700

    formatting

--- 2 - func_collision.py
+++ 2 - func_collision.py
@@ -1,50 +1,53 @@
-import pygame,sys
+import pygame
+import sys
+
 
 pygame.init()
-screen = pygame.display.set_mode((800,800))
+screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 
-moving_surf = pygame.Surface((50,50))
-moving_rect = moving_surf.get_rect(center = (300,300))
-obstacle_surf = pygame.image.load('alpha.png').convert_alpha()
-obstalce_pos = (100,100)
+moving_surf = pygame.Surface((50, 50))
+moving_rect = moving_surf.get_rect(center=(300, 300))
+obstacle_surf = pygame.image.load("alpha.png").convert_alpha()
+obstalce_pos = (100, 100)
 
-# mask 
+# mask
 moving_mask = pygame.mask.from_surface(moving_surf)
 obstacle_mask = pygame.mask.from_surface(obstacle_surf)
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-	screen.fill('white')
+    screen.fill("white")
 
-	# obstacle 
-	screen.blit(obstacle_surf,obstalce_pos)
-	
-	# moving part
-	if pygame.mouse.get_pos(): moving_rect.center = pygame.mouse.get_pos()
-	screen.blit(moving_surf,moving_rect)
+    # obstacle
+    screen.blit(obstacle_surf, obstalce_pos)
 
-	# offset
-	offset_x = obstalce_pos[0] - moving_rect.left
-	offset_y = obstalce_pos[1] - moving_rect.top
-	
-	# collision 1 point
-	if moving_mask.overlap(obstacle_mask,(offset_x,offset_y)):
-		moving_surf.fill('red')
-		# print(moving_mask.overlap(obstacle_mask,(offset_x,offset_y)))
-	else:
-		moving_surf.fill('green')
+    # moving part
+    if pygame.mouse.get_pos():
+        moving_rect.center = pygame.mouse.get_pos()
+    screen.blit(moving_surf, moving_rect)
 
-	# # collision area
-	# if moving_mask.overlap_area(obstacle_mask,(offset_x,offset_y)) > 500:
-	# 	moving_surf.fill('red')
-	# 	#print(moving_mask.overlap_area(obstacle_mask,(offset_x,offset_y)))
-	# else:
-	# 	moving_surf.fill('green')
+    # offset
+    offset_x = obstalce_pos[0] - moving_rect.left
+    offset_y = obstalce_pos[1] - moving_rect.top
 
-	pygame.display.update()
-	clock.tick(60)
+    # collision 1 point
+    if moving_mask.overlap(obstacle_mask, (offset_x, offset_y)):
+        moving_surf.fill("red")
+        # print(moving_mask.overlap(obstacle_mask,(offset_x,offset_y)))
+    else:
+        moving_surf.fill("green")
+
+    # # collision area
+    # if moving_mask.overlap_area(obstacle_mask,(offset_x,offset_y)) > 500:
+    # 	moving_surf.fill('red')
+    # 	#print(moving_mask.overlap_area(obstacle_mask,(offset_x,offset_y)))
+    # else:
+    # 	moving_surf.fill('green')
+
+    pygame.display.update()
+    clock.tick(60)
